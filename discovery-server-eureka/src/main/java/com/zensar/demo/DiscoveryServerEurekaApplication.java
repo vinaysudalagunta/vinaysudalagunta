3b2fbeb6b21447e54d5e2f7e0fd698956987299e
vinaysudalagunta <105418856+vinaysudalagunta@users.noreply.github.com>
2022-06-26 20:31:04 +0530

    Discovery-Server-Eureka

---
+++ discovery-server-eureka/src/main/java/com/zensar/demo/DiscoveryServerEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.zensar.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerEurekaApplication.class, args);
+	}
+
+}
---
+++ discovery-server-eureka/src/test/java/com/zensar/demo/DiscoveryServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
